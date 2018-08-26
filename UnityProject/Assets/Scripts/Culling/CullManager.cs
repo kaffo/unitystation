4999de6ed5ff8c3e73791c8dcc2e2eb55bd59a58
Doobly <dooblynoobly@smashingpixels.com.au>
2018-08-26 16:02:46 +1000

    Start of new culling system

---
+++ UnityProject/Assets/Scripts/Culling/CullManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CullManager : MonoBehaviour
+{
+
+	private static CullManager cullManager;
+
+	public static CullManager Instance
+	{
+		get
+		{
+			if (cullManager == null)
+			{
+				cullManager = FindObjectOfType<CullManager>();
+			}
+			return cullManager;
+		}
+	}
+
+	public static HashSet<BasicTile> baseTiles = new HashSet<BasicTile>();
+
+	private void OnEnable()
+	{
+		SceneManager.activeSceneChanged += Instance.OnSceneChange;
+	}
+
+	private void OnDisable()
+	{
+		SceneManager.activeSceneChanged -= Instance.OnSceneChange;
+	}
+
+	void OnSceneChange(Scene prevScene, Scene nextScene)
+	{
+		ResetManager();
+	}
+
+	void ResetManager()
+	{
+		Debug.Log("Reset");
+		baseTiles.Clear();
+	}
+}
